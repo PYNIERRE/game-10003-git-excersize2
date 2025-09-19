e8e4881a90fcdec8fc46fbe32e348cf88b99cb36
PYNIERRE <000969478@mohawkcollege.ca>
2025-09-19 13:55:43 -0400

    added monolith

--- dungeon_warfare1/Program.cs
+++ dungeon_warfare1/Program.cs
@@ -1,5 +1,9 @@
 ﻿using System.ComponentModel.Design;
 using System.Runtime.InteropServices;
+// initial stats
+int coin = 0;
+int health = 3;
+bool unscathed = true;
 
 Console.WriteLine("* Look around?\n* [y/n]"); // y actually does not exist as an option! set it as else like everything else to save on inputs
 
@@ -17,17 +21,26 @@ else
 }   // witch intro
 Console.ReadLine();
 Console.WriteLine("* You start roaming the path you know best before you know it. A large, tall, horrible \nstone tower is now apparent where you stand, looming far above you -- it's dark silhouette now piercing the smog, and casting over your frail, pale bones."); Console.ReadLine();
-Console.WriteLine("*A shrill voice startles you from behind. You frantically swing behind you.\n\"You're quite far out for someone like yourself, little guy. Haven't seen one of you out here.\""); Console.ReadLine();
+Console.WriteLine("*A sudden shrill, rough voice startles you from behind. You frantically swing behind you.\n\"You're quite far out for someone like yourself, little guy. Haven't seen one of you out here.\""); Console.ReadLine();
 Console.WriteLine("* It's a tall, large green witch.\nShe's fully equipped with a tattered black robe, a buckle, and a floppy pointed hat.\nShe's holding a decently sized log in her hands with an axe strapped around her shoulder. Hardened and ready for battle."); Console.ReadLine();
-Console.WriteLine("\"Who are you, little one?\"");
+Console.WriteLine("\"Who are you, little one?\""); Console.ReadLine();
+Console.WriteLine("[ENTER NAME]");
 string name = Console.ReadLine();
 Console.WriteLine("* You try to speak, but no words cross your barren teeth."); Console.ReadLine();
-Console.WriteLine("\"" +name+ "...  Did someone give you a name?\n* [n/n]"); Console.ReadLine();
-Console.WriteLine("* You almost attempt to say no.. Ah, no vocal chords, right. You shake your head.\n\"Hmm. Someone had to've cast a spell on you. Normally skeletons don't have consciousness..?\""); Console.ReadLine();
+Console.WriteLine("\"" +name+ "...  Did someone give you a name?\"\n* [n/n]");
+input = Console.ReadLine();
+if (input == "")
+{
+    Console.WriteLine("\"A confused little guy. I see.\"");
+}
+else
+{
+    Console.WriteLine("* You almost attempt to say no.. Ah, no vocal chords, right. You shake your head.\n\"Hmm. Someone had to've cast a spell on you. Normally skeletons don't have consciousness..?\""); Console.ReadLine();
+}
 Console.WriteLine("\"Well, I figure you'll need some help little buddy. I can't accompany you, but I can give you this.\""); Console.ReadLine();
 Console.WriteLine("<< You got a coin pouch! It holds 20¢. >>"); Console.ReadLine();
 bool coinpouch = true; //note 4 later, can be stolen by thief and given to merchant. storage method of holding coins! without it you cant store coins on your bones
-int coin = 20;
+coin += 20;
 Console.WriteLine("\"Give that whole pouch to the merchant if you see em'. Tell him I said hi, and to stop being such a bucket-head on his 'sides' debate. I'm not fighting anymore.\""); Console.ReadLine();
 Console.WriteLine("* The lone witch starts hunkering away into the fog."); Console.ReadLine();
 Console.WriteLine("[1] Nod and wave\n[2] Stare");
@@ -36,17 +49,44 @@ int choice = int.Parse (choicestring);
 if (choice == 1)
 {
     Console.WriteLine("* You affirm, waving at the witch goodbye, appreciatively."); Console.ReadLine();
-    Console.WriteLine("* She waves back despite looking nowhere but forward.");
+    Console.WriteLine("* She waves back, trekking into the unknown.");
 }
 else if (choice == 2)
 {
-    Console.WriteLine("* You simply stare at the witch as she leaves, maybe awestruck. Is everyone going to be this friendly?"); Console.ReadLine();
-    Console.WriteLine("* She shakes her head, descending once more into the monochrome haze that surrounds you.");
+    Console.WriteLine("* You simply stare at the witch as she leaves."); Console.ReadLine();
+    Console.WriteLine("* She shakes her head, descending once more into the monochrome haze.");
 }
-else
+else 
 {
     Console.WriteLine("* You couldn't think of anything to do, so you just sat there. She leaves."); Console.ReadLine();
     Console.WriteLine("* You were so focused on finding other thoughts.. that your coin pouch fell out of your ribcage and trinkled a couple coins into the grass. You hurriedly pick it up."); Console.ReadLine();
     coin = coin-2; //note to self make this a random variable from 2 to 5
     Console.WriteLine("<< You now have "+(coin)+ "¢. (-2) >>");
+}
+Console.ReadLine();
+Console.WriteLine("* You ready yourself to enter the monolith. You feel whatever's causing everything weird would be at the root of it. Here I go!"); Console.ReadLine();
+Console.Clear();
+// monolith
+// 2 sets, maybe 3 if i find the extra time                                         ----------------------------------------------------------------------------------------------------------
+// 3 different multi choice options, and a special encounter at the end (i.e. thief)                    ^ creating underline for fancy text                                                   d <- text for sans serif measurement
+Console.WriteLine("░▒▓██████████████▓▒░ ░▒▓██████▓▒░░▒▓███████▓▒░ ░▒▓██████▓▒░░▒▓█▓▒░      ░▒▓█▓▒░▒▓████████▓▒░▒▓█▓▒░░▒▓█▓▒░ \r\n░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░  ░▒▓█▓▒░   ░▒▓█▓▒░░▒▓█▓▒░ \r\n░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░  ░▒▓█▓▒░   ░▒▓█▓▒░░▒▓█▓▒░ \r\n░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░  ░▒▓█▓▒░   ░▒▓████████▓▒░ \r\n░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░  ░▒▓█▓▒░   ░▒▓█▓▒░░▒▓█▓▒░ \r\n░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░  ░▒▓█▓▒░   ░▒▓█▓▒░░▒▓█▓▒░ \r\n░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓██████▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓██████▓▒░░▒▓████████▓▒░▒▓█▓▒░  ░▒▓█▓▒░   ░▒▓█▓▒░░▒▓█▓▒░ \r\n                                                                                                          \r\n----------------------------------------------------------------------------------------------------------\nSTAGE 1/3");
+Console.WriteLine("* You are now in the monolith. You hear chaos above you, but the only way to proceed is up the surrounding spiral stairs."); Console.ReadLine();
+Console.WriteLine("The stairs ahead of you look dangerous. You feel like you should proceed upwards with caution.");
+Console.WriteLine("  1  2  3  4  5"+"\n");
+Console.WriteLine("^ ▓▓ ░░ ▓▓ ▓▓ ██ ^");
+Console.WriteLine("| ▓▓ ▓▓ ██ ▓▓ ▓▓ |");
+Console.WriteLine("| ██ ▓▓ ▓▓ ▓▓ ▒▓ |");
+Console.WriteLine("| ▓▓ ▓▒ ▓▓ ██ ▓▓ |");
+Console.WriteLine("| ▓▓ ▓▓ ██ ▓▓ ░░ |");
+Console.WriteLine("       .(). <- you");
+Console.WriteLine("Maybe you should pre-plan your path? [ENTER ORDER]");
+input = Console.ReadLine();
+if (input == "34135")
+{
+    Console.WriteLine("* You tiptoe across different bricks, weighing their riskiness and condition.");
+}
+else
+{
+    Console.WriteLine("* You have no idea what you're doing. Curse you hand-eye coordination!!!"); Console.ReadLine();
+    Console.WriteLine("* An arrow emerges from the wall, hitting you straight in the skull. Ow!");
 }
